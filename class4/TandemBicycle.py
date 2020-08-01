6ef10614ee3b9ac4bb6b5507ce12cae0d12c16e6
stellaligit <stellaligit@users.noreply.github.com>
2020-08-01 15:02:10 -0400

    Reduce duplicate code.
    Yay! This is the updated version of TandemBicyle.py. Thanks

--- class4/TandemBicycle.py
+++ class4/TandemBicycle.py
@@ -1,37 +1,21 @@
 q = int(input())
 n = int(input())
 
-t = input()
-b1 = t.split()
-a1 = []
-for s in b1:
-    a1.append(int(s))
-a1.sort()
-
-t = input()
-b2 = t.split()
-a2 = []
-for s in b2:
-    a2.append(int(s))
-a2.sort()
-
-ans1 = 0
-for i in range(n):
-    if a1[i] > a2[i]:
-        ans1 = ans1 + a1[i]
-    else:
-        ans1 = ans1 + a2[i]
-
-a1.sort(reverse = True)
-ans2 = 0
-
-for i in range(n):
-    if a1[i] > a2[i]:
-        ans2 = ans2 + a1[i]
-    else:
-        ans2 = ans2 + a2[i]
+a_arr = []
+for i in range(2):
+    t = input()
+    b = t.split()
+    a = [int(s) for s in b]
+    a.sort()
+    a_arr.append(a)
             
-if q == 1:
-    print(ans1)
-else:
-    print(ans2)
+if q != 1:
+    a_arr[1].sort(reverse = True)
+
+ans = 0
+for i in range(n):
+    if a_arr[0][i] > a_arr[1][i]:
+        ans = ans + a_arr[0][i]
+    else:
+        ans = ans + a_arr[1][i]
+print(ans)
