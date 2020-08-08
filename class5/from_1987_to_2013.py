973593309304572a727318648f81f4221f51f1ae
stellaligit <stellaligit@users.noreply.github.com>
2020-08-07 21:46:24 -0400

    from_1987_to_2013.py
    This is the updated form of the older form. Thank you

--- class5/from_1987_to_2013.py
+++ class5/from_1987_to_2013.py
@@ -3,16 +3,7 @@ n = int(input())
 while True:
     n = n + 1
     a = list(str(n))
-    t = []
-    flag = False
-
-    for i in a:
-        if i in t:
-            flag = True
-            break
-        else:
-            t.append(i)
-    
-    if not flag:
+     
+    if len(a) == len(set(a)):
         print(n)
-        break   
+        break
