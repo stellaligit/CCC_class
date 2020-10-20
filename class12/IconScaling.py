c3c5405e127a0471671a7d4fbcfd30d143f0168c
stellaligit <stellaligit@users.noreply.github.com>
2020-10-19 22:48:33 -0400

    Create IconScaling.py
    daddy wants me to write short so hi bye thanks.

---
+++ class12/IconScaling.py
@@ -0,0 +1,38 @@
+k = int(input())
+
+'''
+icon = [
+    "*x*",
+    " xx",
+    "* *"
+]
+
+def printBlock(line):
+    for m in range(3):
+        for i in range(k):
+            for j in range(k):
+                print(line[m][0], end="")
+            for j in range(k):
+                print(line[m][1], end="")
+            for j in range(k - 1):
+                print(line[m][2], end="")
+            print(line[m][2])
+
+printBlock(icon)
+'''
+
+
+def printBlock(line):
+    for i in range(k):
+        for j in range(k):
+            print(line[0], end="")
+        for j in range(k):
+            print(line[1], end="")
+        for j in range(k - 1):
+            print(line[2], end="")
+        print(line[2])
+
+
+printBlock("*x*")
+printBlock(" xx")
+printBlock("* *")
