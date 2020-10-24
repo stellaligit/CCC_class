9d12d499cb10829ae7bfe5ac86afab644b8001b2
stellaligit <stellaligit@users.noreply.github.com>
2020-10-23 23:46:23 -0400

    Update bla bla bla gottado this quick it's so late
    AHHH I HAVE NO CLUE HOW TO DO THISSSS PLEASE HELP, SOMEONE!!!!!!!!!! Anyways this is what i wrote lol and it's pretty late so bye bye. Thanks

--- class12/GoingBackToTheDefinitions.py
+++ class12/GoingBackToTheDefinitions.py
@@ -1,9 +1,30 @@
 n = int(input())
 
+
+class numStr(str):
+    def __eq__(self, other):
+        return super().__eq__(self, other)
+
+    def __ne__(self, other):
+        return super().__ne__(self, other)
+
+    def __gt__(self, other):
+        return self + other > other + other
+
+    def __lt__(self, other):
+        return self + other < other + other
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+
 a = []
 for i in range(n):
     t = input()
-    a.append(t)
+    a.append(numStr(t))
 
 a.sort(reverse=True)
 print(*a, sep="")
