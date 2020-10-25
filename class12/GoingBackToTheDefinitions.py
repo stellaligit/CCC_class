e68929f6da6db5c38f742981e6177c34415a7704
stellaligit <stellaligit@users.noreply.github.com>
2020-10-24 23:11:46 -0400

    Update class11.ipynb and update GoingBackToTheDefinitions.py too
    So I just changed a little teensy tiny change (basicallt just deleted a empy cell lol) and updated GoingBackToTheDeifinitions.py. AND GUESS WHAT EVERYONE? IT'S FINALLY CORRECT!!!!!!!!!!!!!! SO HAPP :DDDD (yes happ not happy happ just sounds nice). I will show you my happy dance: *tap tap tap!! te tap tap!* Hehehe thanks.

--- class12/GoingBackToTheDefinitions.py
+++ class12/GoingBackToTheDefinitions.py
@@ -2,23 +2,38 @@ n = int(input())
 
 
 class numStr(str):
+    def cmp(self, other):
+        s1 = str(self)
+        s2 = str(other)
+        len1 = len(s1)
+        len2 = len(s2)
+        max_test = len1 * len2
+        for i in range(max_test):
+            p1 = i % len1
+            p2 = i % len2
+            if s1[p1] > s2[p2]:
+                return 1
+            elif s1[p1] < s2[p2]:
+                return -1
+        return 0
+
     def __eq__(self, other):
-        return super().__eq__(self, other)
+        return self.cmp(other) == 0
 
     def __ne__(self, other):
-        return super().__ne__(self, other)
+        return self.cmp(other) != 0
 
     def __gt__(self, other):
-        return self + other > other + other
+        return self.cmp(other) == 1
 
     def __lt__(self, other):
-        return self + other < other + other
+        return self.cmp(other) == -1
 
     def __ge__(self, other):
-        return self.__gt__(other) or self.__eq__(other)
+        return self.cmp(other) >= 0
 
     def __le__(self, other):
-        return self.__lt__(other) or self.__eq__(other)
+        return self.cmp(other) <= 0
 
 
 a = []
