e05c94331858d1404eaf306725f4f1b09f3df90d
stellaligit <stellaligit@users.noreply.github.com>
2020-11-14 22:53:15 -0500

    Create semafori.py
    Same as before and before before and before before before... bla bla bla. Thank you (that was VERY short but ya I have a lot of work to do hehe)

---
+++ class10/semafori.py
@@ -0,0 +1,21 @@
+n, m = [int(s) for s in input().split()]
+
+last_position = 0
+leave_time = 0
+for i in range(n):
+    d, r, g = [int(s) for s in input().split()]
+    arrive_time = leave_time + d - last_position
+    mod = arrive_time % (r + g)
+
+    leave_time = arrive_time if mod >= r else leave_time += r - mod
+
+    '''
+    wait_time = 0
+    if mod < r:
+        wait_time = r - mod
+    leave_time = arrive_time + wait_time
+    '''
+
+    last_position = d
+
+print(leave_time + m - last_position)
