62a7d74b7cb9251fe775dbe2dfabfd901c74705d
stellaligit <stellaligit@users.noreply.github.com>
2020-12-19 22:34:09 -0500

    Create MouseMove.py
    Your welcome. Finally some good and interesting thing to look at. Hehe sorry about those days... you see, because of the super annoying pile of work I needa complete each day, I don't have much time to do real good programming but I'm on winter break so yay for us I don't have school homework so I can do technology once again!!! Thank you.

---
+++ class14/MouseMove.py
@@ -0,0 +1,27 @@
+c, r = [int(i) for i in input().split()]
+
+position = [0, 0]
+while True:
+    x, y = [int(i) for i in input().split()]
+    if x == 0 and y == 0:
+        break
+
+    # determining x
+    test = position[0] + x
+    if 0 < test <= c:
+        position[0] = test
+    elif test > c:
+        position[0] = c
+    else:
+        position[0] = 0
+
+    # determining y
+    test = position[1] + y
+    if 0 < test <= r:
+        position[1] = test
+    elif test > r:
+        position[1] = r
+    else:
+        position[1] = 0
+
+    print(*position)
