6adbe799f8975fb1ad96b47e36ea7ae1d432a376
stellaligit <stellaligit@users.noreply.github.com>
2021-01-17 19:05:06 -0500

    Create art.py
    Answer to art.py. Eeeeek I'm excited for tomorrow, do you know why? Well, you'll know tomorrow! ;)

---
+++ level2/class1/art.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+smallest_x = 0
+biggest_x = 0
+smallest_y = 0
+biggest_y = 0
+
+for i in range(n):
+    c = [int(i) for i in input().split(",")]
+    if i == 0:
+        smallest_x = c[0]
+        biggest_x = c[0]
+        smallest_y = c[1]
+        biggest_y = c[1]
+
+    if c[0] < smallest_x:
+        smallest_x = c[0]
+    elif c[0] > biggest_x:
+        biggest_x = c[0]
+
+    if c[1] < smallest_y:
+        smallest_y = c[1]
+    elif c[1] > biggest_y:
+        biggest_y = c[1]
+
+print(smallest_x - 1, smallest_y - 1, sep=",")
+print(biggest_x + 1, biggest_y + 1, sep=",")
