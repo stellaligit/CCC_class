a07e9856f9e26d5d71d909c3b76286c121216287
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-02-05 23:21:04 -0500

    Create EvenMoreOddPhotos.py
    And here, my friends, is my solution. Thank you. Bye!
    
    Audience: ...
    The one and only person: *Claps*
    
    Huh. You could at least pretend to show some enthusiasm >:(
    
    Audience: Greeeeeat speech! Heh, heh, heh...
    
    On second though, nevermind.
    
    Audience: Okie! If you are done with your speech, it's finally time to get my bubble tea! Mmmmm...
    
    Hey, can I have some? Pretty please?
    
    Audience: *Already drinking their bubble tea a block away from the studio.
    
    Awwww, come on! I was really hoping you'd give me one...

---
+++ level2/class3/EvenMoreOddPhotos.py
@@ -0,0 +1,30 @@
+n = int(input())
+a = [int(i) for i in input().split()]
+
+even = 0
+odd = 0
+for i in a:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+if even > odd:
+    num = odd * 2
+else:
+    num = even * 2
+
+if even == odd:
+    print(num)
+elif even > odd:
+    print(num + 1)
+elif even + 1 == odd:
+    print(num - 1)
+else:
+    x = odd - even
+    if x % 3 == 0:
+        print(x * 2//3 + num)
+    elif x % 3 == 1:
+        print((x-1) * 2//3  + num - 1)
+    elif x % 3 == 2:
+        print((x+1) * 2//3 + num - 1)
