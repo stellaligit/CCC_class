baf500281a96c5ed57fb946c48c7d49e1c646e63
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-02-14 21:19:36 -0500

    Update AFactoringProblem.py
    Good news: This won't time out anymore.
    Bad news: I've just realized that this and the one i gave you yesterday were both wrong. Idk whats wrong though.
    Conclusion: Yea. That's about it. Thank you.

--- level2/class3/AFactoringProblem.py
+++ level2/class3/AFactoringProblem.py
@@ -1,8 +1,15 @@
 n = int(input())
+copy = n
 
 ans = 0
-for i in range(2, n//2 + 1):
-    count = 0   
+count = 0   
+while n % 2 == 0:
+    n = n//2
+    count += 1
+ans += 2 * count
+
+for i in range(3, n//2 + 1, 2):
+    count = 0
     while n % i == 0:
         n = n//i
         count += 1
@@ -10,4 +17,6 @@ for i in range(2, n//2 + 1):
     if n == 1:
         break
 
+if ans == 0:
+    ans = copy
 print(ans)
