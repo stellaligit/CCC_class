6956e1f2170c530097c6173d00d06c47b91fd037
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-02-28 12:02:31 -0500

    Update OnlineShopping.py
    This works! Yay! Thank you.

--- level2/class4/OnlineShopping.py
+++ level2/class4/OnlineShopping.py
@@ -1,21 +1,23 @@
 t = int(input())
 
-a = {}
-total = 0
 for i in range(t):
+    a = {}
     n = int(input())
     for j in range(n):
         m = int(input())
         for k in range(m):
             s, p, q = input().split()
             info = a.get(s, [])
-            info.append((p, q))
+            info.append((int(p), int(q)))
             a[s] = info
+    
+    total = 0
     k = int(input())
     for j in range(k):
         t, r = input().split()
         r = int(r)
         info = a[t]
+        info.sort(key=lambda x: x[0])
         for storeInventory in info:
             price = int(storeInventory[0])
             quantity = int(storeInventory[1])
@@ -24,3 +26,5 @@ for i in range(t):
                 total += price * quantity
             else:
                 total += r * price
+                break
+    print(total)
