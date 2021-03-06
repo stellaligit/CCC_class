422f925270e165bdc2b03dd878a7ba8586501b1d
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-03-06 16:16:15 -0500

    Update CountTheTriplets.py
    This works, but it will time-out. (im still figuring out how to solve this). thank you.

--- level2/class5/CountTheTriplets.py
+++ level2/class5/CountTheTriplets.py
@@ -2,7 +2,20 @@ n = int(input())
 a = [int(i) for i in input().split()]
 a.sort()
 
+list_1 = []
+list_2 = []
+for i in a:
+    if i not in list_1:
+        list_1.append(i)
+    elif i not in list_2:
+        list_2.append(i)
+a = list_1
+
 count = 0
+for i in range(len(list_2)):
+    if 2 * list_2[i] in a:
+        count += 1
+
 for i in range(len(a)):
     for j in range(i + 1, len(a)):
         b = a[i] + a[j]
