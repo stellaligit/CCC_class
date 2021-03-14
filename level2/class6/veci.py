5ea012068802061c2fd0b75c6ec528bc87e25d87
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-03-13 20:33:52 -0500

    Update veci.py
    This is a complete answer now but it's not right yet. i'll try to fix it tmr. thank you.

--- level2/class6/veci.py
+++ level2/class6/veci.py
@@ -4,12 +4,29 @@ def veci(arr):
     if len(arr) == 2:
         if arr[0] < arr[1]:
             return arr[1] * 10 + arr[0]
+        else:
+            return 0
     elif len(arr) < 2:
         return 0
     num = arr[0]
-    arr = arr.remove(arr[0])
+    arr.remove(arr[0])
     test = veci(arr)
     if test != 0:
         test += num * (10 ** len(arr))
+        return test
     else:
-        pass
+        arr.sort()
+        for i in range(len(arr)):
+            if arr[i] > num:
+                temp = arr[i]
+                arr[i] = num
+                num = temp
+                break
+        else:
+            return 0
+    
+        for i in range(len(arr)):
+            num = 10 * num + arr[i]
+        return num
+
+print(veci(x))
