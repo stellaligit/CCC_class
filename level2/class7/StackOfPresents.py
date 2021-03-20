40a79cc91718ea625b4eca9566b980bc1f96bed8
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-03-19 22:03:58 -0400

    Create StackOfPresents.py
    This is the answer to stack of presents. thank you.
    
    p.s. i posted the question yesterday

---
+++ level2/class7/StackOfPresents.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+stack = []
+for i in range(n):
+    t = int(input())
+    stack.append(t)
+stack.sort()
+
+if n <= 1:
+    print(n)
+elif n == 2:
+    if stack[0] == stack[1]:
+        print("1")
+    else:
+        print("2")
+else:
+    count = 2
+    sum = stack[0] + stack[1]
+    while True:
+        for i in stack:
+            if sum <= i:
+                count += 1
+                sum += i
+        if sum > stack[-1]:
+            break
+
+    print(count)
