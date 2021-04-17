77ee9d7cba85b76de3bb890f5b2703b70971e009
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-04-16 23:39:41 -0400

    Update FreeShirts.py
    ok so i updated a tiny bit more. thank you.

--- level2/class11/FreeShirts.py
+++ level2/class11/FreeShirts.py
@@ -2,3 +2,5 @@ for i in range(10):
     n, m, d = [int(j) for j in input().split()]
     events = [int(j) for j in input().split()]
     clean_clothes = n
+    for j in range(1, d + 1, 1):
+        pass
