d0dee6c10c368dab3c5422a05106e6e4b5cc7414
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-04-17 20:49:07 -0400

    Update FreeShirts.py
    kay this is a complete solution now. thank you.

--- level2/class11/FreeShirts.py
+++ level2/class11/FreeShirts.py
@@ -2,5 +2,14 @@ for i in range(10):
     n, m, d = [int(j) for j in input().split()]
     events = [int(j) for j in input().split()]
     clean_clothes = n
-    for j in range(1, d + 1, 1):
-        pass
+    total_clothes = n
+    ans = 0
+    for j in range(1, d + 1):
+        if clean_clothes == 0:
+            ans += 1
+            clean_clothes = total_clothes
+        clean_clothes -= 1
+        if j in events:
+            clean_clothes += 1
+            total_clothes += 1
+    print(ans)
