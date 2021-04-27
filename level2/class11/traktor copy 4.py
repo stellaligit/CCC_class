cc7bacd9f5e5e69bf3c2bfc00fd4f0251829e241
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-04-27 11:21:22 -0400

    Create traktor copy 4.py
    dis is yet anoser solusin. sank youzies.

---
+++ level2/class11/traktor copy 4.py
@@ -0,0 +1,54 @@
+n, k = [int(i) for i in input().split()]
+
+x_map = {}
+y_map = {}
+s_map = {}
+d_map = {}
+
+ans = -1
+for i in range(n):
+    x, y = [int(i) for i in input().split()]
+
+    v = x
+    t_map = x_map
+    t = t_map.get(v, 0)
+    if t == k - 1:
+        if i + 1 < ans or ans == -1:
+            ans = i + 1
+        break
+    else:
+        t_map[v] = t + 1
+
+    v = y
+    t_map = y_map
+    t = t_map.get(v, 0)
+    if t == k - 1:
+        if i + 1 < ans or ans == -1:
+            ans = i + 1
+        break
+    else:
+        t_map[v] = t + 1
+
+    v = x + y
+    t_map = s_map
+    t = t_map.get(v, 0)
+    if t == k - 1:
+        if i + 1 < ans or ans == -1:
+            ans = i + 1
+        flag = True
+        break
+    else:
+        t_map[v] = t + 1
+
+    v = x - y
+    t_map = d_map
+    t = t_map.get(v, 0)
+    if t == k - 1:
+        if i + 1 < ans or ans == -1:
+            ans = i + 1
+        flag = True
+        break
+    else:
+        t_map[v] = t + 1
+
+print(ans)
