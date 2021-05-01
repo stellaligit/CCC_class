f4351d87121f798ff60d8c66a334d32c99481239
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-04-30 23:18:46 -0400

    Update TakeANumber.py
    this is my full solution. thank you.

--- level2/class12/TakeANumber.py
+++ level2/class12/TakeANumber.py
@@ -1,3 +1,27 @@
 # Int 1 = Number of "takes" during that day
 # Int 2 = Number of "takes" minus number of "serves" during that day
 # Int 3 = N plus number of total "takes"
+
+n = int(input())
+num_of_takes = 0
+total_num_of_takes = n
+num_of_serves = 0
+while True:
+    s = input()
+    if s == "EOF":
+        break
+
+    if s == "TAKE":
+        if n == 999:
+            n = 1
+        else:
+            n += 1
+        
+        num_of_takes += 1
+        total_num_of_takes += 1
+    elif s == "SERVE":
+        num_of_serves += 1
+    else:
+        print(num_of_takes, num_of_takes - num_of_serves, total_num_of_takes % 999)
+        num_of_serves = 0
+        num_of_takes = 0
