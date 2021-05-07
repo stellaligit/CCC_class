34bc4b6c32c8c71e9d61ca3af9141887b2f1820a
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-05-07 12:13:29 -0400

    Create PlayfulPlaydoughs copy.py
    this is another solution. it also works, but when I submit it, its still TLE.

---
+++ level2/class13/PlayfulPlaydoughs copy.py
@@ -0,0 +1,18 @@
+import math
+n, q = [int(i) for i in input().split()]
+playdoughs = [int(i) for i in input().split()]
+m = {}
+for i in playdoughs:
+    m[i] = m.get(i, 0) + 1
+
+for j in range(q):
+    a, b = [int(i) for i in input().split()]
+    if a == 1:
+        x, y = math.ceil(b/2), math.floor(b/2)
+        t = m.get(b, 0)
+        m[b] = 0
+        if t > 0:
+            m[x] = m.get(x, 0) + t
+            m[y] = m.get(y, 0) + t
+    elif a == 2:
+        print(m.get(b, 0))
