cc4a6074c8a26c0292076adbd17b4bdca172e4ed
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-05-07 12:11:46 -0400

    Update PlayfulPlaydoughs.py
    ok now this one works, but it's too slow. thank you.

--- level2/class13/PlayfulPlaydoughs.py
+++ level2/class13/PlayfulPlaydoughs.py
@@ -1,20 +1,17 @@
 import math
 n, q = [int(i) for i in input().split()]
 playdoughs = [int(i) for i in input().split()]
-for i in range(q):
-    t = [int(i) for i in input().split()]
-    a, b = t[0], t[1]
+m = {}
+for i in playdoughs:
+    m[i] = m.get(i, 0) + 1
+
+for j in range(q):
+    a, b = [int(i) for i in input().split()]
     if a == 1:
         x, y = math.ceil(b/2), math.floor(b/2)
-        for j in playdoughs:
-            if j == b:
-                playdoughs.remove(j)
-                playdoughs.append(x)
-                playdoughs.append(y)
-        continue
+        t = m.get(b, 0)
+        m[b] = 0
+        m[x] = m.get(x, 0) + t
+        m[y] = m.get(y, 0) + t
     elif a == 2:
-        count = 0
-        for j in playdoughs:
-            if j == b:
-                count += 1
-    print(count)
+        print(m.get(b, 0))
