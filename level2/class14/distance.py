8eef7c783fe122a906532e9e98f046a607fc080c
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-05-12 22:03:08 -0400

    Update distance.py
    this is an updated version of my previous solution for distance. thank you.

--- level2/class14/distance.py
+++ level2/class14/distance.py
@@ -1,10 +1,8 @@
 n = int(input())
-if n % 2 == 0:
-    for i in range(1, n // 2 + 1):
-        print(i, end = " ")
-        print(n - i + 1, end = " ")
-else:
-    print("1", end = " ")
-    for i in range(1, (n - 1) // 2 + 1):
-        print(n - i + 1, end = " ")
-        print(i + 1, end = " ")
+
+curr = 1
+direction = 1
+for step in range(n-1, -1, -1):
+    print(curr)
+    curr += step * direction
+    direction = 0 - direction
