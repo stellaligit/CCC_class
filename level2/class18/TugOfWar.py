f3cb1ce3c91492531c32a602b1f8f11d64c57b9e
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-06-11 23:08:17 -0400

    Update TugOfWar.py
    this is the start for my answer on tug of war. idk, i might delete all of this later. depends on how i write my code. thank you.

--- level2/class18/TugOfWar.py
+++ level2/class18/TugOfWar.py
@@ -1,2 +1,14 @@
 n = int(input())
-s = [int(i) for i in input().split()]
+s1 = [int(i) for i in input().split()]
+ans = []
+
+for i in range(n):
+    s2 = s1
+    dif = 0
+    if i == 0:
+        for j in range(s2):
+            sum = 0
+            for k in range(s2[j + 1:-1]):
+                sum += k
+        if dif > abs(sum - s2[0]):
+            dif = abs(sum - s2[0])
