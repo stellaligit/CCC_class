da771ebc3d0a1127815b0e57a3d5752f411b3ba6
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-06-12 17:04:00 -0400

    Update TugOfWar.py
    ok the last commit is actually wrong, i thought it'd be TLE so i wrote that before actually submitted the code to see. this code works now, but it will time out.

--- level2/class18/TugOfWar.py
+++ level2/class18/TugOfWar.py
@@ -1,3 +1,5 @@
+# Mark: 1/3
+
 n = int(input())
 s1 = [int(i) for i in input().split()]
 ans = []
@@ -6,7 +8,7 @@ sum1 = sum(s1)
 for i in range(n):
     s2 = s1[i:n] + s1[0:i]
     dif = sum1
-    for j in range(1, n - 1):
+    for j in range(1, n):
         sum2 = sum(s2[0:j])
         sum3 = sum(s2[j:n])
         dif2 = abs(sum3 - sum2)
