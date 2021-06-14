9a602077e9213f89cb5c7db83f8dd3e1654a0b31
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-06-14 18:59:22 -0400

    Create TugOfWar copy 2.py
    ok this doesn't work either. this will probably be my last one, im just going to move on and start doing this week's homework. thank you.

---
+++ level2/class18/TugOfWar copy 2.py
@@ -0,0 +1,19 @@
+n = int(input())
+s1 = [int(i) for i in input().split()]
+ans = []
+sum1 = sum(s1)
+
+for i in range(n):
+    s2 = s1[i:n] + s1[0:i]
+    dif = sum1
+    previous_sum = 0
+    for j in range(1, n):
+        sum2 = previous_sum + s2[j - 1]
+        previous_sum = sum2
+        sum3 = sum1 - sum2
+        dif2 = abs(sum3 - sum2)
+        if dif > dif2:
+            dif = dif2
+    ans.append(dif)
+
+print(*ans)
