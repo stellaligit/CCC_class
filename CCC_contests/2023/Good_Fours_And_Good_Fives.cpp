69d840d0ad1e25d169bc917358857650e5f2c574
stellaligit <68718208+stellaligit@users.noreply.github.com>
2024-06-09 18:06:26 -0400

    Update Good_Fours_And_Good_Fives.cpp
    I reviewed this problem today and found a few other solutions that should be faster when run.

--- CCC_contests/2023/Good_Fours_And_Good_Fives.cpp
+++ CCC_contests/2023/Good_Fours_And_Good_Fives.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 int func(int n) {
     int ans = 0;
-    int m = n/5;
+    int m = n/5; 
     for (int i = 0; i <= m; i++) {
         if ((n - 5 * i) % 4 == 0) {
             ans += 1;
         }
     }
-
     return ans;
 }
 
@@ -25,6 +24,44 @@ int func(int n) {
 }
 
 */
+
+/*
+int func(int n) {
+    int m = n/5;
+    int r = n % 5;
+    if (r == 1) {
+        m -= 3;
+    } else if (r == 2) {
+        m -= 2;
+    } else if (r == 3) {
+        m -= 1;
+    }
+    if (m < 0) {
+        return 0;
+    } else {
+        return m % 4 + 1;
+    }
+}
+
+*/
+
+int func(int n) {
+    int m = n / 5;
+    int r = n % 5;
+    switch (r) {
+        case 1: m--;
+        case 2: m--;
+        case 3: m--;
+    }
+    return m<0 ? 0 : (m%4)+1;
+}
+
+int func(int n) {
+    int m = n / 5;
+    int r = n % 5;
+    if (r != 0 ) m -= (4-r);
+     return m<0 ? 0 : (m%4)+1;
+}
 
 int main() {
     cout << func(21) << endl;
